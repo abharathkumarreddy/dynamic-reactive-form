3d8c7d94a5d779b70cd7d70ea2d8ad2f464ab3ae
bharath <bharathkumarreddy.a@gmail.com>
2021-05-25 01:03:27 +0530

    initial set up

---
+++ projects/dynamic-reactive-form/src/lib/dynamic-reactive-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tk-dynamic-reactive-form',
+  template: `
+    <p>
+      dynamic-reactive-form works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DynamicReactiveFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/dynamic-reactive-form/src/lib/dynamic-reactive-form.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { DynamicReactiveFormComponent } from './dynamic-reactive-form.component';
+
+
+
+@NgModule({
+  declarations: [DynamicReactiveFormComponent],
+  imports: [
+  ],
+  exports: [DynamicReactiveFormComponent]
+})
+export class DynamicReactiveFormModule { }
